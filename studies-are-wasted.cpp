fea89a3805344a4196a2eb24dcbe67569208e47f
Chris DeVisser <chris.n.devisser@gmail.com>
2019-10-03 07:07:44 -0400

    Optimize the hell out of it
    Instead of doing a graph thingy with every study, studies are grouped by layer in the tree. There's one result per "powerset" of each layer instead of one result per element of said "powerset". Rather than revisiting nodes a million times and getting the same results in a different order, everything is linear. The only problem with this is that things like 302 have a bit less flexibility with when they can be bought, but there is still a very healthy spread of results for every TT value.

--- studies-are-wasted.cpp
+++ studies-are-wasted.cpp
@@ -1,34 +1,38 @@
 #include <algorithm>
 #include <experimental/iterator>
-#include <functional>
+// #include <functional>
 #include <initializer_list>
 #include <iomanip>
 #include <iostream>
 #include <iterator>
+#include <optional>
 #include <sstream>
-#include <unordered_map>
-#include <unordered_set>
 #include <utility>
 #include <vector>
 
 struct Study {
-    int id;
     double cost;
     double cost_multiplier;
-    std::vector<int> prereq_id_options;
 };
 
-struct Node {
+struct StudyLayer {
+    int layer_id;
     Study study;
-    std::vector<int> reachable_ids;
+    int size;
+
+    struct PrereqInfo {
+        std::optional<int> number_unlocked_per_prev_layer;
+        int prev_layer_index_offset = -1;
+    } prereq_info;
 };
 
-struct Graph {
-    Node start;
+struct LayerResult {
+    int layer_id;
+    int quantity;
 };
 
-struct PathCost {
-    std::unordered_set<int> id_path;
+struct Result {
+    std::vector<LayerResult> layer_results;
     double cost;
 };
 
@@ -37,135 +41,97 @@ void debug(const Ts&... ts) {
     // (std::cout << ... << ts) << '\n';
 }
 
-std::string to_path_string(const std::unordered_set<int>& id_path) {
+std::string to_path_string(const std::vector<LayerResult>& layer_results) {
     using std::begin;
     using std::end;
 
-    std::vector<int> path(begin(id_path), end(id_path));
-    std::sort(begin(path), end(path));
-
     std::ostringstream oss;
-    std::copy(begin(path), end(path), std::experimental::ostream_joiner(oss, ','));
+    for (const auto& result : layer_results) {
+        oss << result.quantity << "x" << result.layer_id << " ";
+    }
     return oss.str();
 }
 
-auto get_path_costs(const std::unordered_map<int, Node>& tree, int start_id) -> std::vector<PathCost> {
-    std::vector<PathCost> path_costs;
-    std::unordered_set<int> visited_ids;
+void process_layer(
+    const std::vector<StudyLayer>& tree,
+    int index,
+    std::vector<Result>& results,
+    std::vector<LayerResult>& ongoing_result,
+    double cost_so_far,
+    double cost_multiplier_so_far
+) {
+    if (index >= tree.size()) return;
 
-    double cost = 0;
-    double cost_multiplier = 1;
+    const auto& layer = tree[index];
 
-    std::function<void(const Node&)> process = [&](const Node& node) {
-        if (visited_ids.count(node.study.id)) {
-            debug("Already visited ", node.study.id, " - returning");
-            return;
+    // Case of not buying any in this layer and continuing on.
+    // Especially important for 302.
+    ongoing_result.push_back({layer.layer_id, 0});
+    process_layer(tree, index + 1, results, ongoing_result, cost_so_far, cost_multiplier_so_far);
+    ongoing_result.pop_back();
+
+    auto max = [&] {
+        if (layer.prereq_info.number_unlocked_per_prev_layer) {
+            auto prev_layer_quantity = ongoing_result.at(ongoing_result.size() + layer.prereq_info.prev_layer_index_offset).quantity;
+            auto max_from_prev = *layer.prereq_info.number_unlocked_per_prev_layer * prev_layer_quantity;
+            return std::min(layer.size, max_from_prev);
         }
 
-        debug("Visiting ", node.study.id);
-        cost += node.study.cost * cost_multiplier;
-        cost_multiplier *= node.study.cost_multiplier;
-        visited_ids.insert(node.study.id);
+        return layer.size;
+    }();
+    for (int quantity = 1; quantity <= max; ++quantity) {
+        cost_so_far += layer.study.cost * cost_multiplier_so_far;
+        cost_multiplier_so_far *= layer.study.cost_multiplier;
 
-        path_costs.push_back({visited_ids, cost});
-        debug("Added path: ", to_path_string(visited_ids));
-
-        for (auto id : node.reachable_ids) {
-            debug("Trying out ", id, " as next node");
-            bool has_prereq = std::any_of(begin(node.study.prereq_id_options), end(node.study.prereq_id_options), [&](auto prereq_id) {
-                return visited_ids.count(prereq_id);
-            });
-
-            if (has_prereq or node.study.prereq_id_options.empty()) {
-                process(tree.at(id));
-            } else {
-                debug("Skipping, ", id, " because no prereq is met");
-            }
-        }
-
-        visited_ids.erase(node.study.id);
-        cost_multiplier /= node.study.cost_multiplier;
-        cost -= node.study.cost;
-
-        debug("Backtracking through ", node.study.id);
-    };
-
-    process(tree.at(start_id));
-    return path_costs;
+        ongoing_result.push_back({layer.layer_id, quantity});
+        results.push_back({ongoing_result, cost_so_far});
+        process_layer(tree, index + 1, results, ongoing_result, cost_so_far, cost_multiplier_so_far);
+        ongoing_result.pop_back();
+    }
 }
 
-std::vector<int> concat(std::initializer_list<std::vector<int>> vecs) {
-    using std::begin;
-    using std::end;
+auto get_path_costs(const std::vector<StudyLayer>& tree) -> std::vector<Result> {
+    std::vector<Result> results;
+    results.reserve(1<<12);
 
-    std::vector<int> result;
-    for (const auto& vec : vecs) {
-        result.insert(end(result), begin(vec), end(vec));
-    }
-
-    return result;
+    std::vector<LayerResult> ongoing_result;
+    process_layer(tree, 0, results, ongoing_result, 0, 1);
+    return results;
 }
 
 int main() {
-    std::vector layer250s{251, 252, 253};
-    std::vector layer260s{261, 262, 263, 264, 265, 266};
-    std::vector layer270s{271, 272, 273};
-    std::vector layer280s{281, 282};
-    std::vector layer290s{291, 292};
-    std::vector layer300s{301, 302, 303};
-    std::vector layer310s{311, 312};
-    std::vector layer320s{321, 323};
-
-    auto layer250s_260s = concat({layer250s, layer260s});
-    auto layer260s_270s = concat({layer260s, layer270s});
-    auto layer270s_280s = concat({layer270s, layer280s});
-    auto layer280s_290s_302 = concat({layer280s, layer290s, {302}});
-    auto layer290s_300s = concat({layer290s, layer300s});
-    auto layer300s_310s = concat({layer300s, layer310s});
-    auto layer310s_320s = concat({layer310s, layer320s});
-
-    std::unordered_map<int, Node> mastery_tree{
-        // {ID, {{ID, cost, mult, {prereq options}}, {reachable IDs}}
-        {241, {{241, 1e71, 1}, layer250s}},
-        {251, {{251, 2e71, 2.5, {241}}, layer250s_260s}},
-        {252, {{252, 2e71, 2.5, {241}}, concat({layer250s_260s, layer270s})}},
-        {253, {{253, 2e71, 2.5, {241}}, layer250s_260s}},
-        {261, {{261, 5e71, 6, {251}}, layer260s_270s}},
-        {262, {{262, 5e71, 6, {251}}, layer260s_270s}},
-        {263, {{263, 5e71, 6, {252}}, layer260s_270s}},
-        {264, {{264, 5e71, 6, {252}}, layer260s_270s}},
-        {265, {{265, 5e71, 6, {253}}, layer260s_270s}},
-        {266, {{266, 5e71, 6, {253}}, layer260s_270s}},
-        {271, {{271, 2.74e76, 2, {252}}, layer270s_280s}},
-        {272, {{272, 2.74e76, 2, {252}}, concat({layer270s, layer280s_290s_302})}},
-        {273, {{273, 2.74e76, 2, {252}}, layer270s_280s}},
-        {281, {{281, 6.86e76, 4, {271, 272}}, layer280s_290s_302}},
-        {282, {{282, 6.86e76, 4, {272, 273}}, layer280s_290s_302}},
-        {291, {{291, 2.14e77, 1, {272}}, /*layer290s_300s*/}},
-        {292, {{292, 2.14e77, 1, {272}}, /*layer290s_300s*/}},
-        // {301, {{301, 8.57e77, 2, {291}}, layer300s_310s}},
-        {302, {{302, 8.57e77, 2, {272}}, /*layer300s_310s*/}},
-        // {303, {{303, 8.57e77, 2, {292}}, layer300s_310s}},
-        // {311, {{311, 8.57e77, 64, {301}}, layer310s_320s}},
-        // {312, {{312, 8.57e77, 64, {303}}, layer310s_320s}},
-        // {321, {{321, 2.68e76, 2, {311}}, layer320s}},
-        // {323, {{323, 2.68e76, 2, {312}}, layer320s}}
+    std::vector<StudyLayer> layers{
+        // {ID, {cost, mult}, size, {unlocks per prev, prev index offset}}
+        {240, {1e71, 1}, 1},
+        {250, {2e71, 2.5}, 3, {3}},
+        {260, {5e71, 6}, 6, {2}},
+        {270, {2.74e76, 2}, 3, {3, -2}},
+        {280, {6.86e76, 4}, 2, {2}},
+        {290, {2.14e77, 1}, 2, {2, -2}},
+        {300, {8.57e77, 2}, 2, {1}},
+        {302, {2.68e78, 1<<17}, 1, {1, -4}},
+        {310, {8.57e77, 64}, 2, {1, -2}},
+        {320, {2.68e76, 2}, 2, {1}},
     };
 
     using std::begin;
     using std::end;
 
-    auto path_costs = get_path_costs(mastery_tree, 241);
+    auto path_costs = get_path_costs(layers);
     std::sort(begin(path_costs), end(path_costs), [](const auto& lhs, const auto& rhs) {
         return lhs.cost < rhs.cost;
     });
 
     std::cout << std::scientific << std::setprecision(2);
     double last_cost = 1;
+    int display_count = 0;
     for (const auto& path_cost : path_costs) {
         if (path_cost.cost / last_cost > 1.01) {
-            std::cout << path_cost.cost << ": " << to_path_string(path_cost.id_path) << "|0\n";
+            std::cout << path_cost.cost << ": " << to_path_string(path_cost.layer_results) << "\n";
             last_cost = path_cost.cost;
+            ++display_count;
         }
     }
+
+    std::cout << display_count << " out of " << path_costs.size() << " shown";
 }
